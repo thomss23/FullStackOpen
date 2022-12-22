037fe0537f8fa1def2aaea122e0226cb0bfb089b
Thomas Borbeli <thomasborbeli@Thomass-MacBook-Air.local>
2022-12-22 23:41:35 +0200

    finish ex 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </>
-  
-const Course = ({course}) => {
-  return (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={course.parts.reduce((acumulator, currentvalue) => currentvalue.exercises + acumulator, 0)} />
-  </div>)
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
